ca81f659fe18d0a00268db689b1fe5abe38d9e3f
JoBov86 <jnbovington@gmail.com>
2021-09-29 11:18:14 +0100

    style header toggle icon

--- includes/header.php
+++ includes/header.php
@@ -26,7 +26,7 @@
         <div class="container-fluid">
           <a class="navbar-brand" href="index.php">IT Conference</a>
           <button       
-          class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          class="navbar-toggler navbar-dark" style="border: solid 2px #0066CC;" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse justify-content-between" id="navbarNavAltMarkup">
